7a875e985f17da05e6bc4dfa0fc624986586b683
Lee Ji Seok <76767226+dev-jiseok@users.noreply.github.com>
2021-02-21 19:46:00 +0900

    2021-02-21

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
+function Food({ fav }) {
+  return <h3>i like {fav}, maybe?</h3>;
+}
+
 function App() {
   return <div>
     <h1>Hello! jiseok</h1>
+    <Food fav='kimchi' />
+    <Food fav='kimbap' />
+    <Food fav='apple' />
+    <Food fav='mintchoco' />
+
   </div>;
 }
 
