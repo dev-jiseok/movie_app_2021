6a7c506048fe75d367cba0d2de3b6ed9b6568d77
Lee Ji Seok <76767226+dev-jiseok@users.noreply.github.com>
2021-02-28 15:15:52 +0900

    2021-02-28

--- src/routes/About.js
+++ src/routes/About.js
@@ -2,15 +2,15 @@ import React from "react";
 import "./About.css";
 
 function About(props) {
-          console.log(props);
-          return (
-                    <div className="about__container">
-                              <span>
-                                        “Freedom is the freedom to say that two plus two make four. If that is
-                                        granted, all else follows.”
+  console.log(props);
+  return (
+    <div className="about__container">
+      <span>
+        “Freedom is the freedom to say that two plus two make four. If that is
+        granted, all else follows.”
       </span>
-                              <span>− George Orwell, 1984</span>
-                    </div>
-          );
+      <span>− George Orwell, 1984</span>
+    </div>
+  );
 }
 export default About;
